98846787af77b0d33898028ab2d200ca3bb3ef5c
felipemarinhodev <felipmarinho@gmail.com>
2023-04-14 20:24:14 -0700

    test: :white_check_mark: add: ensure will check the feature flag using unleash

--- src/test/routes/eventosRoutes.test.js
+++ src/test/routes/eventosRoutes.test.js
@@ -2,46 +2,47 @@
 import { after } from 'mocha';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import sinon from 'sinon';
 import app from '../../app.js';
 import db from '../../db/dbconfig.js';
+import EventosController from '../../controllers/eventosController.js';
 
 chai.use(chaiHttp);
 const { expect } = chai;
+
+// eslint-disable-next-line no-unused-vars
+let stub;
 
 after(async () => {
   await db.destroy();
 });
 
-describe('GET em /eventos', () => {
-  beforeEach(() => {
-    process.env.EVENTO_FLAG = 'true';
-  });
+it('Deve retornar uma lista de eventos', (done) => {
+  stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(true);
+  chai.request(app)
+    .get('/eventos')
+    .set('Accept', 'application/json')
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body[0]).to.have.property('id');
+      expect(res.body[0]).to.have.property('nome');
+      expect(res.body[0]).to.have.property('data');
+      expect(res.body[0]).to.have.property('descricao');
+      expect(res.body[0]).to.have.property('created_at');
+      expect(res.body[0]).to.have.property('updated_at');
+      done();
+    });
+});
 
-  it('Deve retornar uma lista de eventos', (done) => {
-    chai.request(app)
-      .get('/eventos')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.an('array');
-        expect(res.body[0]).to.have.property('id');
-        expect(res.body[0]).to.have.property('nome');
-        expect(res.body[0]).to.have.property('data');
-        expect(res.body[0]).to.have.property('descricao');
-        expect(res.body[0]).to.have.property('created_at');
-        expect(res.body[0]).to.have.property('updated_at');
-        done();
-      });
-  });
-
-  it('Deve retornar erro 404', (done) => {
-    process.env.EVENTO_FLAG = 'false';
-    chai.request(app)
-      .get('/eventos')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        expect(res.status).to.equal(404);
-        done();
-      });
-  });
+it('Deve retornar erro 404', (done) => {
+  stub.restore();
+  stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(false);
+  chai.request(app)
+    .get('/eventos')
+    .set('Accept', 'application/json')
+    .end((err, res) => {
+      expect(res.status).to.equal(404);
+      done();
+    });
 });
